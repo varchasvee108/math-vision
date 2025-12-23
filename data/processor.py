3141cbd03b1690d212081d3c1aa262cb42957628
varchasvee <varchasvee108@gmail.com>
2025-12-23 15:12:13 +0530

    processor.py added

--- data/processor.py
+++ data/processor.py
@@ -1,3 +1,7 @@
+import torch
+from torchvision import transforms
+from PIL import Image
+
 VOCAB = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "<EOS>", "<PAD>"]
 
 stoi = {s: i for i, s in enumerate(VOCAB)}
@@ -5,3 +9,28 @@ itos = {i: s for i, s in enumerate(VOCAB)}
 
 PAD_ID = stoi["<PAD>"]
 EOS_ID = stoi["<EOS>"]
+
+
+class VisionMathProcessor:
+    def __init__(self, image_size):
+        self.image_transform = transforms.Compose(
+            [
+                transforms.Resize(image_size),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.5], std=[0.5]),
+            ]
+        )
+
+        def preprocess_image(self, img):
+            return self.image_transform(img)
+
+        def decode(self, token_ids):
+            if isinstance(token_ids, torch.Tensor):
+                token_ids = token_ids.tolist()
+
+            chars = []
+            for t in token_ids:
+                if t in (EOS_ID, PAD_ID):
+                    break
+                chars.append(itos(t))
+            return "".join(chars)
